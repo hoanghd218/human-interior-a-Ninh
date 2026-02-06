702993bf3f1d4bb65fe80f5d3f1501115b5eaaf1
linhtran08 <vaiolinh@outlook.com>
2026-02-06 15:50:37 +0700

    fix(maps): update embed URL to show marker clearly
    - Fixed Google Maps embed URL format to display marker
    - Coordinates: 11.0695783, 106.6797949 (MIDORI PARK)

--- humaninterior/src/components/landing/ShowroomSection.tsx
+++ humaninterior/src/components/landing/ShowroomSection.tsx
@@ -27,7 +27,7 @@ const ShowroomSection = () => {
 
                     <div className="relative rounded-xl md:rounded-2xl overflow-hidden border border-gray-200 shadow-2xl aspect-video md:aspect-[21/9]">
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.0123!2d106.6775949!3d11.0695783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174d1a3e1e1e1e1%3A0x1234567890abcdef!2sMIDORI%20PARK!5e0!3m2!1sen!2s!4v1738741000000!5m2!1sen!2s"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62688.19!2d106.6797949!3d11.0695783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTHCsDA0JzEwLjUiTiAxMDbCsDQwJzQ3LjMiRQ!5e0!3m2!1svi!2s!4v1738742000000!5m2!1svi!2s"
                             width="100%"
                             height="100%"
                             style={{ border: 0 }}
